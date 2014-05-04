6570fb52fdfcf7b7e7cd9a07f0132f99e9237e8c
Danilo Jr <danilobjr@gmail.com>
2014-05-03 22:09:32 -0300

    fixed error handling

--- server/services/email.js
+++ server/services/email.js
@@ -10,12 +10,15 @@ module.exports.sendMessage = function (messageOptions, callback) {
     "message": messageOptions,
     "async": async
   }, function(result) {
-    console.log(result);
+    if (result[0].status === 'rejected') {
+      return callback(new Error(result[0].reject_reason));
+    }
     callback(null, result);
+    console.log(result);
   }, function(err) {
+    callback(err);
     // Mandrill returns the error as an object with name and message keys
     console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message); // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
-    callback(err);
   });
 };
 
